cd7ef9878a51ff90213e7cd5ba14983f1dd8fdd4
Steven Israel <malenchite@gmail.com>
2020-12-14 08:57:25 -0500

    Created question list object array with initial question set

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var questionList = [
+  {
+    question: "All lines should be terminated with a ________",
+    answers: ["semicolon", "period", "ampersand", "backslash"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which of these variable types can be stored in arrays?",
+    answers: ["numbers", "strings", "booleans", "all of the above"],
+    correctAnswer: 4
+  },
+  {
+    question: "What character is used to bracket the contents of an array?",
+    answers: ["quotation mark", "square bracket", "curly bracket", "angle bracket"],
+    correctAnswer: 2
+  },
+  {
+    question: "Which of these functions displays a message to the user with only an 'OK' button?",
+    answers: ["prompt()", "confirm()", "check()", "alert()"],
+    correctAnswer: 4
+  },
+  {
+    question: "What function do you call to print something to the console?",
+    answers: ["console.log()", "console()", "log()", "print()"],
+    correctAnswer: 1
+  },
+  {
+    question: "Which of the following will check if the variable 'x' is equal in both value and type to the variable 'y'?",
+    answers: ["x = y", "x == y", "x === y", "x != y"],
+    correctAnswer: 3
+  },
+];
