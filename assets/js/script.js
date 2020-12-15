e08ca307a9f5463842da8163c1f68ba8b2019d5d
Steven Israel <malenchite@gmail.com>
2020-12-14 19:16:16 -0500

    Updated initial state along with event handler for start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,9 +31,18 @@ var questionList = [
   },
 ];
 
+var viewScoresDiv = document.getElementById("view-high-scores");
+var resultDiv = document.getElementById("result-block");
+var timerDiv = document.getElementById("timer-div");
+var startBtn = document.getElementById("start-btn");
+
+var currentQuestion;
+
 /* Updates the question and answer text to the specified question index */
 function updateQuestion(qIdx) {
   var questionText = document.getElementById("question-text");
+
+  currentQuestion = qIdx;
 
   questionText.textContent = questionList[qIdx].question;
   
@@ -57,6 +66,27 @@ function showID(id) {
   element.setAttribute("style", "");
 }
 
+/* Set initial state */
 hideID("question-page");
+hideID("result-block");
 hideID("complete-page");
 hideID("high-score-page");
+timerDiv.setAttribute("style", "visibility:hidden");
+
+/* Add listener to start quiz when Start button is clicked */
+startBtn.addEventListener("click", function() {
+  /* Hide 'View High Scores' */
+  viewScoresDiv.setAttribute("style", "visibility: hidden");
+
+  /* Show Timer */
+  timerDiv.setAttribute("style", "visibility: visible");
+
+  /* Hide start page */
+  hideID("start-page");
+
+  /* Load first question */
+  updateQuestion(0);
+
+  /* Show question page */
+  showID("question-page");
+});
